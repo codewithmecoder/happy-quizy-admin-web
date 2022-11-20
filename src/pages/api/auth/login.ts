2c8396629c735bddfc5d412d64afee27f38248a9
yaty1031 <yaty1031@gmail.com>
2022-11-20 13:08:29 +0700

    fix dep

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -8,37 +8,35 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  switch (req.method) {
-    case 'POST':
-      const user = await loginUser(req.body);
-      const data = user.data as BaseResponse<UserTokenModel>;
-      const date = new Date();
-      const cookie = new Cookie(req, res, {
-        secure: process.env.NODE_ENV !== 'development',
-      });
-      cookie.set('accessToken', data?.data.accessToken, {
-        // maxAge: 900000, //15 mins
-        maxAge: 1000,
-        httpOnly: true,
-        path: '/',
-        sameSite: 'strict',
-      });
-      cookie.set('refreshToken', data?.data.refreshToken, {
-        maxAge: 3.154e10, //1 year
-        httpOnly: true,
-        path: '/',
-        sameSite: 'strict',
-        expires: new Date(
-          new Date().setFullYear(
-            date.getFullYear() + 1,
-            date.getMonth(),
-            date.getDate()
-          )
-        ),
-      });
-      res.status(200).send(data);
-      break;
-    default:
-      break;
+  if (req.method === 'POST') {
+    const user = await loginUser(req.body);
+    const data = user.data as BaseResponse<UserTokenModel>;
+    const date = new Date();
+    const cookie = new Cookie(req, res, {
+      secure: process.env.NODE_ENV !== 'development',
+    });
+    cookie.set('accessToken', data?.data.accessToken, {
+      // maxAge: 900000, //15 mins
+      maxAge: 1000,
+      httpOnly: true,
+      path: '/',
+      sameSite: 'strict',
+    });
+    cookie.set('refreshToken', data?.data.refreshToken, {
+      maxAge: 3.154e10, //1 year
+      httpOnly: true,
+      path: '/',
+      sameSite: 'strict',
+      expires: new Date(
+        new Date().setFullYear(
+          date.getFullYear() + 1,
+          date.getMonth(),
+          date.getDate()
+        )
+      ),
+    });
+    res.status(200).send(data);
+  } else {
+    res.status(405).send({ status: 405 });
   }
 }
--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -4,28 +4,26 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  switch (req.method) {
-    case 'POST':
-      const cookie = new Cookie(req, res, {
-        secure: process.env.NODE_ENV !== 'development',
-      });
-      cookie.set('accessToken', '', {
-        maxAge: 0, //1 year
-        httpOnly: true,
-        path: '/',
-        sameSite: 'strict',
-        expires: new Date(Date.now()),
-      });
-      cookie.set('refreshToken', '', {
-        maxAge: 0, //1 year
-        httpOnly: true,
-        path: '/',
-        sameSite: 'strict',
-        expires: new Date(Date.now()),
-      });
-      res.status(200).send({ status: 200 });
-      break;
-    default:
-      break;
+  if (req.method === 'POST') {
+    const cookie = new Cookie(req, res, {
+      secure: process.env.NODE_ENV !== 'development',
+    });
+    cookie.set('accessToken', '', {
+      maxAge: 0, //1 year
+      httpOnly: true,
+      path: '/',
+      sameSite: 'strict',
+      expires: new Date(Date.now()),
+    });
+    cookie.set('refreshToken', '', {
+      maxAge: 0, //1 year
+      httpOnly: true,
+      path: '/',
+      sameSite: 'strict',
+      expires: new Date(Date.now()),
+    });
+    res.status(200).send({ status: 200 });
+  } else {
+    res.status(405).send({ status: 405 });
   }
 }
