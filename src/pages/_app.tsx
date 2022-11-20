481694d00d1cd5a858633c76419fb810456c25b7
yaty1031 <yaty1031@gmail.com>
2022-11-20 12:42:53 +0700

    add meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '../../styles/globals.css';
 import Navbar from '../components/Navbar';
@@ -9,6 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="bg-zinc-800 min-h-screen min-w-full">
+        <Head>
+          <meta
+            http-equiv="Content-Security-Policy"
+            content="upgrade-insecure-requests"
+          ></meta>
+        </Head>
         <Navbar userData={pageProps.userData} />
         <Component {...pageProps} />
       </div>
